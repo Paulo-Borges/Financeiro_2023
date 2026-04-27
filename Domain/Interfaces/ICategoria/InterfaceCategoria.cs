1962e13a26527ff74aa23af034b9a3cc7a300ab0
Borges <borgespaulo72@yahoo.com.br>
2026-04-27 20:55:32 -0300

    feat: criando os repositorios

--- Domain/Interfaces/ICategoria/InterfaceCategoria.cs
+++ Domain/Interfaces/ICategoria/InterfaceCategoria.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Interfaces.Generics;
+using Entities.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +11,8 @@ namespace Domain.Interfaces.ICategoria
 
 
     //São os crud do projetos 
-    public interface InterfaceCategoria
+    public interface InterfaceCategoria : InterfaceGeneric<Categoria>
     {
+        Task<IList<Categoria>> ListarCategoriaUsuario(string emailUsuario);
     }
 }
