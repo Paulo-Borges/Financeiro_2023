d3db4f92144ff7ac3c61e2ad3bb1b9013c123e58
Borges <borgespaulo72@yahoo.com.br>
2026-04-27 15:01:29 -0300

    feat: iniciando entidades e enum

---
+++ Entities/Entidades/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entidades
+{
+    //tabela pra customizar o Identity 
+    public class ApplicationUser : IdentityUser
+    {
+        [Column("USR_CPF")]
+
+        public string CPF { get; set; }
+    }
+}
---
+++ Entities/Entidades/UsuarioSistemaFinanceiro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entidades
+{
+
+    [Table("UsuarioSistemaFinanceiro")]
+    public class UsuarioSistemaFinanceiro
+    {
+        public int Id { get; set; }
+
+        public string EmailUsuario { get; set; }
+        public int Administrador { get; set; }
+        public int SistemaAtual { get; set; }
+
+        [ForeignKey("SistemaFinanceiro")]
+        [Column(Order = 1)]
+        public int IdSistema { get; set; }
+        public virtual SistemaFinanceiro SistemaFinanceiro { get; set; }
+    }
+}
